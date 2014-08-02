8ab272bfc1fa3d07fd137ff99b5a27daeb509a53
Mat Gadd <mat@zerifas.com>
2014-08-02 23:05:06 +0100

    Add a transformer for boolean values.

---
+++ src/Transformer/Boolean.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zerifas\Supermodel\Transformer;
+
+abstract class Boolean
+{
+    /**
+     * Transform an integer to a bool.
+     *
+     * @param int $int Integer, where 0 is false, all other values are true.
+     *
+     * @return bool
+     */
+    public static function fromInteger($integer)
+    {
+        return ($integer !== 0);
+    }
+
+    /**
+     * Transform a bool to an integer.
+     *
+     * @param bool $bool Boolean value.
+     *
+     * @return int
+     */
+    public static function fromBoolean($bool = null)
+    {
+        if ($bool === null) {
+            return null;
+        }
+
+        return $bool ? 1 : 0;
+    }
+}
