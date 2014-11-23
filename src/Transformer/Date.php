398a8e7052f610c4e796bb4cb558d85f544593b5
Mat Gadd <mat@zerifas.com>
2014-11-23 22:26:28 +0000

    Add Date and Time transformers.

--- src/Transformer/Date.php
+++ src/Transformer/Date.php
@@ -39,6 +39,70 @@ abstract class Date
     }
 
     /**
+     * Transform a 'yyyy-mm-dd' format string to a DateTime.
+     *
+     * @param string $string Date string.
+     *
+     * @return DateTime
+     */
+    public static function fromDateString($string)
+    {
+        if ($string === null) {
+            return null;
+        }
+
+        return DateTime::createFromFormat('Y-m-d', $string);
+    }
+
+    /**
+     * Transform a DateTime to a 'yyyy-mm-dd' format string.
+     *
+     * @param DateTime $datetime DateTime object.
+     *
+     * @return string
+     */
+    public static function fromDate(DateTime $datetime = null)
+    {
+        if ($datetime === null) {
+            return null;
+        }
+
+        return $datetime->format('Y-m-d');
+    }
+
+    /**
+     * Transform a 'hh:mm:ss' format string to a DateTime.
+     *
+     * @param string $string Date string.
+     *
+     * @return DateTime
+     */
+    public static function fromTimeString($string)
+    {
+        if ($string === null) {
+            return null;
+        }
+
+        return DateTime::createFromFormat('H:i:s', $string);
+    }
+
+    /**
+     * Transform a DateTime to a 'yyyy-mm-dd' format string.
+     *
+     * @param DateTime $datetime DateTime object.
+     *
+     * @return string
+     */
+    public static function fromTime(DateTime $datetime = null)
+    {
+        if ($datetime === null) {
+            return null;
+        }
+
+        return $datetime->format('H:i:s');
+    }
+
+    /**
      * Transform an ISO8601 format string for a DateTime.
      *
      * @param string $string Datetime string.
