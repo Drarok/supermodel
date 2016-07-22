dd0d709bfc8b5eeeec3b087fc6239fd6a17244f8
Mat Gadd <mat@zerifas.com>
2016-07-22 12:59:12 +0100

    Break apart the multi-purpose old Date transformer into more specific ones

---
+++ src/Transformer/DateTime.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Zerifas\Supermodel\Transformer;
+
+abstract class DateTime extends AbstractDate
+{
+    const FORMAT_STRING = 'Y-m-d H:i:s';
+}
---
+++ src/Transformer/Time.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Zerifas\Supermodel\Transformer;
+
+abstract class Time extends AbstractDate
+{
+    const FORMAT_STRING = 'H:i:s';
+}
