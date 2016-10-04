ea77bee4458aee7206dc78447794abe420dfd36b
Mat Gadd <mat@zerifas.com>
2016-10-04 16:01:57 +0100

    Use the main class's version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -4,16 +4,14 @@ namespace Zerifas\Supermodel\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
 
+use Zerifas\Supermodel\AbstractModel;
 use Zerifas\Supermodel\Console\Command\GenerateCommand;
 
 class Application extends BaseApplication
 {
-    const NAME = 'Supermodel';
-    const VERSION = '1.0.0';
-
     public function __construct()
     {
-        parent::__construct(static::NAME, static::VERSION);
+        parent::__construct('Supermodel', AbstractModel::VERSION);
 
         $this->addCommands([
             new GenerateCommand(),
