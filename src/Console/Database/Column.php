7e1bf4b907ab4a469feb07fc9e3c88d57919d739
Mat Gadd <mat@zerifas.com>
2016-10-04 15:42:11 +0100

    Add support for getting a column's limit

--- src/Console/Database/Column.php
+++ src/Console/Database/Column.php
@@ -6,19 +6,28 @@ class Column
 {
     protected $name;
     protected $type;
+    protected $limit;
     protected $unsigned;
     protected $null;
 
     public function __construct(array $row)
     {
-        if (! preg_match('/^(\w+)(?:\(\d+\))? ?(unsigned)?$/i', $row['Type'], $matches)) {
+        if (! preg_match('/^(\w+)(?:\(([\d,]+)\))? ?(unsigned)?$/i', $row['Type'], $matches)) {
             throw new \InvalidArgumentException('Failed to parse column type: ' . $row['Type']);
         }
 
         $this->name = $row['Field'];
         $this->type = strtoupper($matches[1]);
+        if (array_key_exists(2, $matches)) {
+            $limit = $matches[2];
+            if (is_numeric($limit)) {
+                $this->limit = intval($limit);
+            } else {
+                $this->limit = $limit;
+            }
+        }
         $this->null = $row['Null'] === 'YES';
-        $this->unsigned = array_key_exists(2, $matches) && strtolower($matches[2]) == 'unsigned';
+        $this->unsigned = array_key_exists(3, $matches) && strtolower($matches[3]) == 'unsigned';
     }
 
     public function getName()
@@ -29,6 +38,11 @@ class Column
     public function getType()
     {
         return $this->type;
+    }
+
+    public function getLimit()
+    {
+        return $this->limit;
     }
 
     public function isUnsigned()
