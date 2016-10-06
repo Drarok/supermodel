7debe96ba17fe14e38f3303b05b8bb0de7bf5595
Mat Gadd <mat@zerifas.com>
2016-10-06 12:30:40 +0100

    Bump version

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -9,7 +9,7 @@ abstract class AbstractModel
     /**
      * Version of Supermodel.
      */
-    const VERSION = '1.2.2';
+    const VERSION = '1.3.0';
 
     /**
      * Array of column names as contained in the _table_. If you want to change
