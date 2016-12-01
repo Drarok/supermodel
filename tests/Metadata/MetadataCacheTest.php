d72b1c0f53db9d981d3f4eb590dde35660bc661d
Mat Gadd <mat@zerifas.com>
2016-12-01 22:10:15 +0000

    Move test model to the models directory

--- tests/Metadata/MetadataCacheTest.php
+++ tests/Metadata/MetadataCacheTest.php
@@ -8,60 +8,7 @@ use Zerifas\Supermodel\Cache\MemoryCache;
 use Zerifas\Supermodel\Metadata\MetadataCache;
 use Zerifas\Supermodel\SupermodelInterface;
 
-class MetadataTestModel implements SupermodelInterface
-{
-    private static $tableName = 0;
-    private static $columns = 0;
-    private static $valueTransformers = 0;
-    private static $relations = 0;
-
-    public static function getTableName(): string
-    {
-        ++self::$tableName;
-        return 'posts';
-    }
-
-    public static function getTableNameCount(): int
-    {
-        return self::$tableName;
-    }
-
-    public static function getColumns(): array
-    {
-        ++self::$columns;
-        return [
-            'id',
-            'name',
-        ];
-    }
-
-    public static function getColumnsCount(): int
-    {
-        return self::$columns;
-    }
-
-    public static function getValueTransformers(): array
-    {
-        ++self::$valueTransformers;
-        return [];
-    }
-
-    public static function getValueTransformersCount(): int
-    {
-        return self::$valueTransformers;
-    }
-
-    public static function getRelations(): array
-    {
-        ++self::$relations;
-        return [];
-    }
-
-    public static function getRelationsCount(): int
-    {
-        return self::$relations;
-    }
-}
+use Zerifas\Supermodel\Test\Model\MetadataTestModel;
 
 class MetadataCacheTest extends TestCase
 {
